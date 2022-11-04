497a84c97b16d9f6165b1e34dccdc7488deeca3e
SQUEK011 <77522687+SQUEK011@users.noreply.github.com>
2022-11-05 07:13:22 +0800

    fix validations

--- html/process_registration.php
+++ html/process_registration.php
@@ -16,39 +16,47 @@
     $addressTwo = $_POST['getAddressTwo'];
     $postal = $_POST['getPostal'];
     $emergencyName = $_POST['getEmergencyName'];
-    $emergencyContact = $_POST['getEsmergencyNumber'];
+    $emergencyContact = $_POST['getEmergencyNumber'];
     $emergencyRelate = $_POST['getEmergencyRelation'];
     $password = $_POST['getPassword'];
 
-  $query = "SELECT EXISTS (SELECT * from user_profile where user_name = '$user' OR email='$email' OR full_name = '$fullname)";
+  $query = "SELECT EXISTS (SELECT * from user_profile where user_name = '$user' OR email='$email' OR full_name = '$fullname' or nric='$nric')";
   if(mysqli_num_rows(mysqli_query($conn,$query)) < 1) {
-    $sql = "INSERT INTO user_accounts(
-      user_name,pass_word,admin_rights)
-      VALUES ('$user','$password',0);";
-
-    if (!mysqli_query($conn, $sql)) {
-      echo "Failed to register: " . mysqli_error($connNew) . ".Please try again.";
+    $query = "SELECT EXISTS (SELECT * from user_profile where full_name = '$fullname' or nric='$nric')";
+    if(mysqli_num_rows(mysqli_query($conn,$query)) < 1){
+      $sql = "INSERT INTO user_accounts(
+        user_name,pass_word,admin_rights)
+        VALUES ('$user','$password',0);";
+  
+      if (!mysqli_query($conn, $sql)) {
+        echo "Failed to register: " . mysqli_error($connNew) . ".Please try again.";
+      }
+  
+      $sql = "INSERT INTO user_profile(
+        user_name,full_name,nric,D_O_B,gender,
+      occupation,mobile_no,email,allergies,address_1,address_2,
+      postal_code,emergency_contact_name,emergency_contact_no,emergency_contact_relation)
+      VALUES ('$user','$fullname','$nric','$dob','$gender','$occupation',
+      '$mobileNumber','$email','$allergies','$addressOne','$addressTwo','$postal',
+      '$emergencyName','$emergencyContact','$emergencyRelate');";
+  
+      if (!mysqli_query($conn, $sql)) {
+        echo "Failed to register: " . mysqli_error($connNew) . ".Please try again.";
+      } else {
+  
+        $_SESSION['use'] = $user;
+  
+        echo '<script type="text/javascript"> alert("Register Successful!");</script>';
+        echo '<script type="text/javascript"> window.open("../index.php","_self");</script>';            //  On Successful Login redirects to home.php
+      }
+    }else {
+      echo '<script type="text/javascript"> alert("Full Name/NRIC already exists!");</script>';
+      echo '<script type="text/javascript"> window.open("registration.php","_self");</script>';   
     }
-
-    $sql = "INSERT INTO user_profile(
-      user_name,full_name,nric,D_O_B,gender,
-    occupation,mobile_no,email,allergies,address_1,address_2,
-    postal_code,emergency_contact_name,emergency_contact_no,emergency_contact_relation)
-    VALUES ('$user','$fullname','$nric','$dob','$gender','$occupation',
-    '$mobileNumber','$email','$allergies','$addressOne','$addressTwo','$postal',
-    '$emergencyName','$emergencyContact','$emergencyRelate');";
-
-    if (!mysqli_query($conn, $sql)) {
-      echo "Failed to register: " . mysqli_error($connNew) . ".Please try again.";
-    } else {
-
-      $_SESSION['use'] = $user;
-
-      echo '<script type="text/javascript"> alert("Register Successful!");</script>';
-      echo '<script type="text/javascript"> window.open("../index.php","_self");</script>';            //  On Successful Login redirects to home.php
-    }
+    
+    
   }else {
-    echo '<script type="text/javascript"> alert("User/Email already exists!");</script>';
+    echo '<script type="text/javascript"> alert("Username/Email already exists!");</script>';
       echo '<script type="text/javascript"> window.open("registration.php","_self");</script>';   
   }
  
